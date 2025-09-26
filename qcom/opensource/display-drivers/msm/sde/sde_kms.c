43ef945f4bd5fc78c96bf9e11099306247a20b33
John Galt <johngaltfirstrun@gmail.com>
2025-09-26 08:00:37 -0400

    display-drivers: sde_kms: utilize kprofiles
    Signed-off-by: GuidixX <guidixm@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- qcom/opensource/display-drivers/msm/sde/sde_kms.c
+++ qcom/opensource/display-drivers/msm/sde/sde_kms.c
@@ -1356,8 +1356,18 @@ static void sde_kms_prepare_commit(struct msm_kms *kms,
 		goto end;
 	}
 
-	cpu_boost_kick(6);
-	qcom_dcvs_bus_boost_kick(6);
+	switch (kp_active_mode()) {
+	case 1:
+		break;
+	case 3:
+		cpu_boost_kick(8);
+		qcom_dcvs_bus_boost_kick(8);
+		break;
+	default:
+		cpu_boost_kick(6);
+		qcom_dcvs_bus_boost_kick(6);
+		break;
+       }
 
 	if (sde_kms->first_kickoff) {
 		sde_power_scale_reg_bus(&priv->phandle, VOTE_INDEX_HIGH, false);
